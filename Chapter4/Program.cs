66cdc5cfee5241b95faf5e12832abfbfa35a724d
HaruneIsoda <31072@ojs.ac.jp>
2020-10-01 10:37:49 +0900

    P117 null条件演算子

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -1,26 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Chapter4 {
     class Program {
         static void Main(string[] args) {
-            string code = "12345";
-
-            var message = GetMessage(code) ?? DefaultMessage();
-            Console.WriteLine(message);
+            Console.WriteLine(GetProduct());
         }
 
-        //スタブ
-        private static object DefaultMessage() {
-            return "DefaultMessage";
+        private static string GetProduct() {
+            Sale sale = new Sale {
+                ShopName = "pet store",
+                Amount = 100000,
+                Product = "food"
+            };
+            sale = null;
+            return sale?.Product;
         }
+    }
 
-        //スタブ
-        private static object GetMessage(string code) {
-            return "555";
-        }
+    class Sale {
+        public string ShopName { get; set; }
+        public int Amount { get; set; }
+        public string Product { get; set; }
     }
 }
