10976e1a82635a85a9410c698f28eedc13e2036b
HaruneIsoda <31072@ojs.ac.jp>
2020-10-09 15:47:57 +0900

    第六章の演習問題

--- Chapter6/Program.cs
+++ Chapter6/Program.cs
@@ -3,49 +3,89 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Chapter6 {
     class Program {
         static void Main(string[] args) {
-            //整数の例
-            var numbers = new List<int> { 19, 17, 15, 24, 12, 25, 14, 20, 12, 28, 19, 30, 24 };
 
-            numbers.Distinct().Select(n => n.ToString("0000")).ToList().ForEach(s => Console.Write(s + " "));
+            #region 6-1
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+            //6-1-1
+            Console.WriteLine($"最大値…{numbers.Max()}");
 
-            Console.WriteLine("\n***************************************************");
-            var strings = numbers.Distinct().ToArray();
-            foreach(var str in strings) {
-                Console.Write(str + " ");
+            //6-1-2
+            Console.Write("最後から2つの要素…");
+            int pos = numbers.Length - 2;
+            foreach(var number in numbers.Skip(pos)) {
+                Console.Write(number + " ");
             }
 
-            Console.WriteLine("\n***************************************************");
-            numbers.Distinct().OrderBy(n => n).ToList().ForEach(n => Console.Write(n + " "));
+            //6-1-3
+            Console.Write("\n文字列に変換…");
+            numbers.Select(x => x.ToString("000")).ToList().ForEach(x => Console.Write(x + " "));
 
-            //文字列の例
-            Console.WriteLine("\n***************************************************");
-            var words = new List<string> { "Microsoht", "Apple", "Google", "Oracle", "Facebook", };
+            //6-1-4
+            Console.Write("\n数の小さい数値を三つ…");
+            numbers.Distinct().OrderBy(x => x).Take(3).ToList().ForEach(x => Console.Write(x + " "));
 
-            var lower = words.Select(s => s.ToLower()).ToArray();
+            //6-1-5
+            Console.Write("\n10より大きい数がいくつあるか…");
+            Console.WriteLine(numbers.Distinct().Count(x => x > 10));
+
+            #endregion
+
+            Console.WriteLine("***************************************************");
+
+            #region 6-2
+            var books = new List<Book> {
+                new Book { Title = "C#プログラミングの新常識", Price = 3800, Pages = 378 },
+                new Book { Title = "ラムダ式とLINQの極意", Price = 2500, Pages = 312 },
+                new Book { Title = "ワンダフル・C#ライフ", Price = 2900, Pages = 385 },
+                new Book { Title = "一人で学ぶ並列処理プログラミング", Price = 4800, Pages = 464 },
+                new Book { Title = "フレーズで覚えるC#入門", Price = 5300, Pages = 604 },
+                new Book { Title = "私でも分かったASP.NET MVC", Price = 3200, Pages = 453 },
+                new Book { Title = "楽しいC#プログラミング教室", Price = 2540, Pages = 348 },
+            };
 
 
-            //オブジェクトの例
-            var books = Books.GetBooks();
-            //タイトルリスト
-            var titles = books.Select(x => x.Title).ToArray();
-            foreach(var title in titles) {
-                Console.Write(title + " ");
+            //6-2-1
+            Console.Write($"ワンダフル・C#ライフ…");
+            foreach(var book in books.Where(x => x.Title == "ワンダフル・C#ライフ")) {
+                Console.WriteLine($"{book.Price}円 {book.Pages}ページ");
             }
 
-            Console.WriteLine("\n***************************************************");
-            //ページの多い順に並べ替え
-            Console.WriteLine("ページの多い順…");
-            books.OrderByDescending(x => x.Pages).Take(3).ToList().ForEach(x => Console.Write($"{x.Title}:{x.Pages}p　"));
+            //6-2-2
+            Console.Write("\n「C#」が含まれている書籍数…");
+            Console.WriteLine(books.Count(x => x.Title.Contains("C#")) + "冊");
 
-            Console.WriteLine("\n金額の高い順…");
-            books.OrderByDescending(x => x.Price).Take(3).ToList().ForEach(x => Console.Write($"{x.Title}:{x.Price}円　"));
+            //6-2-3
+            Console.Write("\n「C#」が含まれている書籍の平均ページ…");
+            Console.WriteLine(books.Where(x => x.Title.Contains("C#")).Average(x => x.Pages) + "ページ");
 
+            //6-2-4
+            Console.Write("\n価格が4000円以上の最初の本…");
+            Console.WriteLine(books.FirstOrDefault(x => x.Price >= 4000).Title);
+
+            //6-2-5
+            Console.Write("\n価格が4000円未満で最大のページ数…");
+            Console.WriteLine(books.Where(x => x.Price < 4000).Max(x => x.Pages) + "ページ");
+
+            //6-2-6
+            Console.WriteLine("\n400ページ以上の書籍を価格の高い順に…");
+            foreach(var book in books.Where(x => x.Pages >= 400).OrderByDescending(x => x.Price)) {
+                Console.WriteLine($"{book.Title}：{book.Price}円　");
+            }
+
+            //6-2-7
+            Console.WriteLine("\n\n「C#」が含まれていて500ページ以下の本…");
+            foreach(var book in books.Where(x => x.Title.Contains("C#") && x.Pages <= 500)) {
+                Console.WriteLine($"{book.Title}　");
+            } 
+            #endregion
         }
     }
 }
