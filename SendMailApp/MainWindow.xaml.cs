ae18a2ef43766aaec6038a2c92674ce699a9a2af
HaruneIsoda <31072@ojs.ac.jp>
2020-11-05 11:02:44 +0900

    複数のメアドへの送信

--- SendMailApp/MainWindow.xaml.cs
+++ SendMailApp/MainWindow.xaml.cs
@@ -39,8 +39,13 @@ namespace SendMailApp {
         //メール送信処理
         private void btOk_Click(object sender, RoutedEventArgs e) {
             try {
-                MailMessage msg = new MailMessage("ojsinfosys01@gmail.com", tbTo.Text);
-
+                MailMessage msg = new MailMessage("ojsinfosys01@gmail.com",tbTo.Text);
+                if(tbCc.Text != "") {
+                    msg.CC.Add(tbCc.Text);
+                }
+                if(tbBcc.Text != "") {
+                    msg.Bcc.Add(tbBcc.Text);
+                }
                 msg.Subject = tbTitle.Text; //件名
                 msg.Body = tbBody.Text; //本文
 
